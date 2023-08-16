a8df9db160ce14f09e75e53220a4f9e79f6314d7
Jefferson <jeffersoncerqueira75@gmail.com>
2023-08-16 03:34:27 -0300

    adding swagger config

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 
 async function bootstrap() {
@@ -11,6 +12,17 @@ async function bootstrap() {
     transform: true,
 
   }))
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle("BestBier")
+    .setTitle("The BestBier API Description")
+    .setVersion("0.1")
+    .addTag("bestbier")
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
